88190088713080fd7cfcf340b01bc70c35c53cc3
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-08-03 11:44:25 +0900

    [FEAT] 앨범 이름으로 검색

--- src/main/java/com/munecting/server/domain/music/dto/get/MusicSearchRes.java
+++ src/main/java/com/munecting/server/domain/music/dto/get/MusicSearchRes.java
@@ -1,12 +1,17 @@
 package com.munecting.server.domain.music.dto.get;
 
+import lombok.Builder;
 import lombok.Getter;
 import se.michaelthelin.spotify.model_objects.specification.Image;
 
 @Getter
 public class MusicSearchRes {
-    private Image[] name;
-    public MusicSearchRes(Image[] name){
+    private String name;            //앨범 제목
+    private String artist;          //가수 이름
+    private String coverImg;        //앨범 이미지
+    public MusicSearchRes(String name,String artist,String coverImg){
         this.name = name;
+        this.artist = artist;
+        this.coverImg = coverImg;
     }
 }
