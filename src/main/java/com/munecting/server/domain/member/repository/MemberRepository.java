a9e15098a157c79ec215bf592beec7599a6d72fe
Sasha <106591445+kchaeys2@users.noreply.github.com>
2023-08-11 11:54:40 +0900

    [FIX] Member 구현 #24

--- src/main/java/com/munecting/server/domain/member/repository/MemberRepository.java
+++ src/main/java/com/munecting/server/domain/member/repository/MemberRepository.java
@@ -2,13 +2,10 @@ package com.munecting.server.domain.member.repository;
 
 import com.munecting.server.domain.member.DTO.MemberDTO;
 import com.munecting.server.domain.member.entity.Member;
-import jakarta.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<MemberDTO, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 }
