98da727807e00c5dd290d509237d4b09ce5f94ff
tkamo2006 <tkamo2006@naver.com>
2023-08-17 20:33:42 +0900

    [FEAT] 로그인 기능 구현 #31

---
+++ src/main/java/com/munecting/server/domain/member/controller/MemberController.java
@@ -0,0 +1,67 @@
+package com.munecting.server.domain.member.controller;
+
+import com.munecting.server.domain.member.DTO.ChangePwInfo;
+import com.munecting.server.domain.member.DTO.MemberDTO;
+import com.munecting.server.domain.member.entity.Member;
+import com.munecting.server.domain.member.service.LoginService;
+import com.munecting.server.domain.member.service.MemberService;
+import com.munecting.server.global.config.BaseResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/users")
+public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private LoginService loginService;
+
+
+    // 유저 정보 조회
+    @GetMapping("")
+    public BaseResponse<Optional<Member>> getMyInfo(HttpServletRequest request) throws Exception {
+        Optional<Member> user = memberService.getMyInfo(request);
+        return new BaseResponse<>(user);
+    }
+
+    // 유저 정보 수정
+    @PostMapping("")
+    public BaseResponse<String> updateMyInfo(@RequestBody MemberDTO user, HttpServletRequest request) throws Exception {
+        memberService.updateMyInfo(user, request);
+        return new BaseResponse<>("정보가 수정되었습니다.");
+    }
+
+    // 유저 비밀번호 확인
+    @PostMapping("/validatepw")
+    public BaseResponse<String> validatePw(@RequestBody Map<String, String> requestBody, HttpServletRequest request) throws Exception {
+        if (loginService.validatePw(requestBody.get("originalPw"), request))
+            return new BaseResponse<>("비밀번호 확인");
+        return new BaseResponse<>("비밀번호 불일치");
+    }
+
+    // 유저 비밀번호 변경
+    @PostMapping("/changepw")
+    public BaseResponse<String> updatePw(@RequestBody ChangePwInfo changePw, HttpServletRequest request) throws Exception {
+        loginService.changePw(changePw, request);
+        return new BaseResponse<>("비밀번호가 수정되었습니다.");
+    }
+
+    // 유저 회원 탈퇴( A -> D )
+    @PostMapping("/delete")
+    public BaseResponse<Optional<Member>> deactivateUser(HttpServletRequest request) throws Exception {
+        Optional<Member> userEntity = memberService.deactivateUser(request);
+        return new BaseResponse<>(userEntity);
+    }
+
+}
